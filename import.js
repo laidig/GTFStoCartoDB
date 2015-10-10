d35faae1828eb8226d39a806d91e9dad59822794
tony <laidig@gmail.com>
2015-10-09 20:37:55 -0400

    a little more functional: now synchronously uploads files such that errors are less likely (works with small GTFS files no problem). uses native unzip-wrapper as AdmZip had issues with a number of test files

--- import.js
+++ import.js
@@ -5,11 +5,44 @@
 var http = require('http'),
     fs = require('fs'),
     request = require('request'),
-    AdmZip = require('adm-zip');
+    //AdmZip has some issues. unzip is a lot more tolerant. 
+    //AdmZip = require('adm-zip'),
+    unzip = require('unzip-wrapper'),
     out = fs.createWriteStream('google_transit.zip'),
     replace = require("replace"),
     walk = require("walk");	
-    config = require('./config.json');
+    config = require('./config.json'),
+    fileUrl = require('file-url'),
+	//options for walk to process synchronously
+ 	options = {
+	    listeners: {
+	      names: function (root, nodeNamesArray) {
+	        nodeNamesArray.sort(function (a, b) {
+	          if (a > b) return 1;
+	          if (a < b) return -1;
+	          return 0;
+	        });
+	      }
+	    , directories: function (root, dirStatsArray, next) {
+	        next();
+	      }
+	      // runs on every file walked over
+	    , file: function (root, fileStats, next) {
+	    	inProcess = getUploadsInProcess();
+
+	    	while (inProcess <3) {
+	   			cartoDBimport(fileStats.name,next());
+	   			sleep(10);
+	   			inProcess = getUploadsInProcess();
+	   			console.log(inProcess, 'imports running');
+	   		}
+
+	      }
+	    , errors: function (root, nodeStatsArray, next) {
+	        next();
+	      }
+	    }
+	  };
 
 var validFiles = [
 	"agency.txt",
@@ -34,11 +67,10 @@ var req = request(
     }
 );
 
+console.log('Starting DL of', config.gtfsUrl);
 req.pipe(out);
 req.on('end', function() {
-    var zip = new AdmZip("google_transit.zip"),
-    zipEntries = zip.getEntries();
-    zip.extractAllTo("unzip/", true);
+    unzip("google_transit.zip", { target : 'unzip/'}),
 
     //todo add some logic to handlr errors
     //processUnzipped();
@@ -46,8 +78,6 @@ req.on('end', function() {
 });
 
 function renameHeaders() {
-
-
 	replace({
 	    regex: "stop_lat",
 	    replacement: "lat",
@@ -70,26 +100,33 @@ function renameHeaders() {
 function processUnzipped() {
 	//iterate over our unzipped files and push each to cartoDB
 
-	var walker  = walk.walk('./unzip', { followLinks: false });
+	var walker  = walk.walkSync('./unzip', options);
 
-	walker.on('file', function(root, stat, next) {
-	    // Add this file to the list of files
-	    console.log(stat.name);
-	    cartoDBimport(stat.name,next());
-	    // next();
-	});
 };
+
+function getUploadsInProcess() {
+	var url = 'https://' + config.username + '.cartodb.com/api/v1/imports/?api_key=' + config.apikey;
+
+	var req = request.get(url, function (err,resp,body){
+		if (err) {
+		     console.log('Error!');
+		 } else {
+		     imports = JSON.parse(body);
+		     console.log(imports);
+		     return imports.imports.length;
+		   }
+	})
+	
+}
 
 function cartoDBimport(filename,callback) {
 	var url = 'https://' + config.username + '.cartodb.com/api/v1/imports/?api_key=' + config.apikey;
-
-
-console.log(url);
-	 var req = request.post(url, function (err, resp, body) {
+	 
+	var req = request.post(url, function (err, resp, body) {
 	   if (err) {
 	     console.log('Error!');
 	   } else {
-	     console.log('Response: ' + body);
+	     //console.log('Response: ' + body);
 	     callback;
 	   }
 	 });
